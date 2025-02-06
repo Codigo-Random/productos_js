73129647d750010e7bd6e6ebda7966224a15f0e1
andrey <andrey@sublimestudio.es>
2025-02-06 11:39:09 +0100

    --wip-- [skip ci]

--- js/auth.js
+++ js/auth.js
@@ -1,8 +1,15 @@
 const APIKEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyaWxlb2ZkamtjbXNwdmVibnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNzk1MDYsImV4cCI6MjA0OTg1NTUwNn0.MZx5Cpcw6aqM7A9Sc8_VC6HWnSKQ0SYkWpTqUAI0-Pg"
-const BASE_URL = "https://irileofdjkcmspvebnqq.supabase.co/"
+const BASE_URL = "https://irileofdjkcmspvebnqq.supabase.co"
 
 const inputEmail = document.getElementById("email");
 const inputPassword = document.getElementById("password");
+
+// Btn de login
+const btnLogin = document.getElementById("login");
+
+if(btnLogin) {
+    btnLogin.addEventListener("click", login)
+}
 
 async function login() {
 
@@ -29,4 +36,37 @@ async function login() {
     localStorage.setItem("userId", result.user.id)
 
     window.location.href = "/dashboard.html"
+}
+
+export async function isUserLogged(access_token, userId) {
+
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            "apikey": APIKEY,
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${access_token}`
+        }
+    };
+
+    const response = await fetch(`${BASE_URL}/auth/v1/user`, requestOptions) 
+    if(!response.ok) {
+        alert("Error al iniciar sesión")
+        return false
+    }
+
+    const result = await response.json()
+
+    if(result.id == userId) {
+        return true
+    }
+ 
+    return false
+}
+
+export function logout() {
+    localStorage.removeItem("token")
+    localStorage.removeItem("userId")
+
+    window.location.href = "/index.html"
 }
--- retricted.js
+++
@@ -1,6 +0,0 @@
-const token = localStorage.getItem("token")
-const userId = localStorage.getItem("userId")
-
-if(!token || !userId) {
-    window.location.href = "/index.html"
-}
