9c4376b12d2ace5c6d186f0c4a713cd6be24243b
andrey <andrey@sublimestudio.es>
2025-02-07 10:02:51 +0100

    --wip-- [skip ci]

--- js/products.js
+++ js/products.js
@@ -6,6 +6,8 @@ if(btnLogout) {
     btnLogout.addEventListener("click", logout)
 }
 
+const productList = document.getElementById("productList");
+
 async function getProducts() {
 
     const requestOptions = {
@@ -26,6 +28,29 @@ async function getProducts() {
     const result = await response.json()
 
     console.log(result)
+    printProducts(result)
+
+}
+
+function printProducts(allProducts) {
+    allProducts.forEach(product => {
+        productList.innerHTML += `
+            <div class="bg-white rounded-lg shadow-md p-6">
+                <h2 class="text-xl font-semibold mb-2">${product.name}</h2>
+                <p class="text-gray-600 mb-2">${product.description}</p>
+                <p class="text-gray-600 mb-2">Categoría: ${product?.categories_clm?.name}</p>
+                <p class="text-lg font-bold text-green-600 mb-4">${product.price}€</p>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                        Editar
+                    </button>
+                    <button class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+                        Eliminar
+                    </button>
+                </div>
+            </div>
+        `
+    });
 }
 
 getProducts()
